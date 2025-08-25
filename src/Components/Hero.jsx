f4087d92c837be289b0a59254544e9b79da8abfd
YAMMN <yassin.ahmed.ya0707@gmail.com>
2025-08-25 15:35:08 +0300

    changed logo source

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import MainLogo from "./assets/logo-OVAC-1.png";
 const Hero = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-[40vh] h-screen px-4 py-8 text-center">
       <img
         className="w-48 sm:w-62 md:w-78 lg:w-86 xl:w-100 h-auto mb-6"
-        src="src/assets/logo-OVAC-1.png"
+        src={MainLogo}
         alt="Hero"
       />
       <p className="text-[#53be97] text-xl sm:text-2xl md:text-3xl font-bold max-w-xl">

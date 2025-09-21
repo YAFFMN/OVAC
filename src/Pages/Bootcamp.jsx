42f493da847f67c7f79434aea3ac5545978e3c10
Tareq-Khalil <tareqkhalil1415@gmail.com>
2025-09-22 01:32:44 +0300

    Update Bootcamp.jsx

--- src/Pages/Bootcamp.jsx
+++ src/Pages/Bootcamp.jsx
@@ -7,17 +7,42 @@ import BootcampStats from '../Components/BootcampStats'
 import BootcampEligibility from '../Components/BootcampEligibility'
 import BootcampProgramContent from '../Components/BootcampProgramContent'
 import BootcampCTA from '../Components/BootcampCTA'
+
 const Bootcamp = () => {
   return (
-    <div className='bg-black min-h-screen'>
-      <NavBar />
-      <StarField />
-      <BootcampHero />
-      <BootcampStats />
-      <BootcampEligibility />
-      <BootcampProgramContent />
-      <BootcampCTA /> 
-      <Footer />
+    <div className='bg-black min-h-screen w-full overflow-x-hidden'>
+      <div className='relative z-50'>
+        <NavBar />
+      </div>
+      <div className='fixed inset-0 z-0'>
+        <StarField />
+      </div>
+      <main className='relative z-10 w-full'>
+        <section className='w-full px-4 sm:px-6 lg:px-8'>
+          <div className='container mx-auto'>
+            <BootcampHero />
+          </div>
+        </section>
+        <section className='w-full px-4 sm:px-6 lg:px-8 py-8 sm:py-12 md:py-16'>
+          <div className='container mx-auto'>
+            <BootcampStats />
+          </div>
+        </section>
+        <section className='w-full px-4 sm:px-6 lg:px-8 py-8 sm:py-12 md:py-16'>
+          <div className='container mx-auto'>
+            <BootcampEligibility />
+          </div>
+        </section>
+        <section className='w-full'>
+          <BootcampProgramContent />
+        </section>
+        <section className='w-full'>
+          <BootcampCTA />
+        </section>
+      </main>
+      <footer className='relative z-10 w-full'>
+        <Footer />
+      </footer>
     </div>
   )
 }

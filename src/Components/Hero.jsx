e201215a3437b49344cbbd1ec57a1b3b1ebc2ab3
Tareq-Khalil <tareqkhalil1415@gmail.com>
2025-09-25 02:23:37 +0300

    Update Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import MainLogo from "../assets/logo-OVAC-1.png";
 
 const Hero = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const bootcamp = () => {
-    navigate('/bootcamp')
-  }
+    navigate('/bootcamp');
+  };
+  
   const gallery = () => {
-    navigate('/gallery')
-  }
+    navigate('/gallery');
+  };
+
   return (
     <div className="flex flex-col items-center justify-start h-screen px-4 pt-28 text-center">
       <img
@@ -27,18 +30,18 @@ const Hero = () => {
       </p>
       {/* buttons  */}
       <div className="mt-8 flex flex-col items-center justify-center gap-6 sm:flex-row">
-        <a
+        <button
           onClick={bootcamp}
           className="transform rounded-full bg-[#ec1a63] px-8 py-4 font-bold text-white shadow-lg shadow-[#ec1a63]/30 transition-all duration-300 ease-in-out hover:scale-105 hover:bg-[#ec1a63] hover:shadow-xl hover:shadow-[#ec1a63]/50"
         >
           Register For the Bootcamp
-        </a>
-        <a
+        </button>
+        <button
           onClick={gallery}
           className="transform rounded-full border-2 border-[#2caa7c] bg-transparent px-8 py-4 font-bold text-[#2caa7c] transition-all duration-300 ease-in-out hover:scale-105 hover:border-[#2caa7c] hover:bg-[#2caa7c] hover:text-white hover:shadow-lg hover:shadow-[#2caa7c]/40"
         >
           See our Art
-        </a>
+        </button>
       </div>
     </div>
   );

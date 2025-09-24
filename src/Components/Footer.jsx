d4b97c39f5c01d5c3ca1b19e4e72c5b4e6dc4fef
Tareq-Khalil <tareqkhalil1415@gmail.com>
2025-09-25 01:43:16 +0300

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,23 +7,27 @@ const Footer = () => {
       <div className="container mx-auto text-center">
         <div className="mb-6">
           <img src={Logo} alt="OVAC Logo" className="mx-auto mb-2 w-24" />
-          <h3 className="text-2xl font-bold text-[var(--main-color-3)]">OVAC</h3>
-          <p className="text-gray-400 mt-2">OVAC Visual Arts Club</p>
+<h3 className="text-2xl font-bold" style={{ fontFamily: 'moto-verse' }}>
+  <span style={{ color: 'white' }}>O</span>
+  <span style={{ color: '#ec1a63' }}>V</span>
+  <span style={{ color: '#fdef9d' }}>A</span>
+  <span style={{ color: '#2caa7c' }}>C</span>
+</h3>          <p className="text-gray-400 mt-2">October Visual Arts Club</p>
         </div>
         <div className="flex justify-center space-x-6 mb-6">
-          <a href="#" className="text-gray-400 hover:text-[var(--main-color-2)] transition-colors duration-300" aria-label="Instagram">
-            <FaInstagram size={24} />
-          </a>
-          <a href="#" className="text-gray-400 hover:text-[var(--main-color)] transition-colors duration-300" aria-label="Twitter">
+          <a href="https://www.youtube.com/channel/UCWVPFF_BouMjRywzfGz6LAg" className="text-gray-400 hover:text-[var(--main-color)] transition-colors duration-300" aria-label="youtube">
             <FaYoutube size={24} />
           </a>
-          <a href="#" className="text-gray-400 hover:text-[var(--main-color-3)] transition-colors duration-300" aria-label="Facebook">
+          <a href="https://www.instagram.com/visual_arts_club.oc?igsh=MTImcml3YWk2NTQxbg==" className="text-gray-400 hover:text-[var(--main-color-2)] transition-colors duration-300" aria-label="Instagram">
+            <FaInstagram size={24} />
+          </a>
+          <a href="https://www.facebook.com/people/Visual-Arts-Club/61558432703807/" className="text-gray-400 hover:text-[var(--main-color-3)] transition-colors duration-300" aria-label="Facebook">
             <FaFacebookF size={24} />
           </a>
         </div>
         {/* adress and mail */}
         <div className="mb-6 text-gray-400">
-          <a href="mailto:visualartsclubstem@gmail.com" className="hover:text-[#ec1a63] transition-colors duration-300">
+          <a href="mailto:visualartsclubstem@gmail.com" className="hover:text-[white] transition-colors duration-300">
               visualartsclubstem@gmail.com
           </a>
         </div>

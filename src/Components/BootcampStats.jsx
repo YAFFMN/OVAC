397247d13fc264525fb4f765eabedb846ccaecbe
Tareq-Khalil <tareqkhalil1415@gmail.com>
2025-09-26 22:54:53 +0300

    Add files via upload

--- src/Components/BootcampStats.jsx
+++ src/Components/BootcampStats.jsx
@@ -116,4 +116,3 @@ const BootcampStats = () => {
 };
 
 export default BootcampStats;
-
---
+++ src/Components/Underline.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Underline = ({ color = "#ec1a63" }) => {
+  return (
+    <div className='w-24 h-1 rounded mb-8' style={{ backgroundColor: color }}>
+    </div>
+  )
+}
+
+export default Underline

4cc3f351a0da1e0df8e7c5ae925ee1060f96917c
YAMMN <yassin.ahmed.ya0707@gmail.com>
2025-09-27 12:44:03 +0300

    fixing 2.0

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import './assets/fonts/fonts.css';
-import './assets/fonts/Motoverse.woff2';
+
 
 
 createRoot(document.getElementById('root')).render(
